9ac1594bae5f511f91f236cafe96f8d62d6e9327
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2023-10-05 16:37:53 -0300

    ThemeContext and themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Routes from "./routes.tsx";
+import { AppThemeProvider } from "./data/contexts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Routes />
+    <AppThemeProvider>
+      <Routes />
+    </AppThemeProvider>
   </React.StrictMode>
 );
