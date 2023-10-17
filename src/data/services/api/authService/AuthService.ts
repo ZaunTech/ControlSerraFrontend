5d77e1610a5eed4973f712cb7ef2b88913b236b0
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2023-10-17 11:08:36 -0300

    Serviço base de auth jwt

---
+++ src/data/services/api/authService/AuthService.ts
@@ -0,0 +1,26 @@
+import { Api } from "..";
+
+interface IAuth {
+  accessToken: string;
+}
+
+const auth = async (
+  email: string,
+  password: string
+): Promise<IAuth | Error> => {
+  try {
+    const { data } = await Api.get("/auth", { data: { email, password } }); //Metodo get apenas para teste, pois nao foi implementado autenticação no mock do json server
+    if (data) {
+      return data.accessToken;
+    }
+    return new Error("Erro ao efetuar o login");
+  } catch (error) {
+    return new Error(
+      (error as { message: string }).message || "Erro ao efetuar o login"
+    );
+  }
+};
+
+export const AuthService = {
+  auth,
+};
