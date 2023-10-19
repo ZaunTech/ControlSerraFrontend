dbc4d8b5d00d683cc4334c195d80eef9b0cb9ddc
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2023-10-19 14:04:13 -0300

    Exports Modules Consultas

--- src/data/services/api/modules/index.ts
+++ src/data/services/api/modules/index.ts
@@ -1,3 +1,4 @@
 export { CategoriasService } from './categorias/Categorias.service'
 export { InsumosService } from './insumos/Insumos.service'
 export { FornecedoresService } from './fornecedores/Fornecedores.service'
+export { ClientesService } from './clientes/ClientesServices'
