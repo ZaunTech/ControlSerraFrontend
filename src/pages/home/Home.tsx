a1c98c1de1dced3b8aa2829aa9d6cbf13558e3db
gaabhenrique <74377524+gaabhenrique@users.noreply.github.com>
2023-10-27 01:41:23 -0300

    Correção dashboard

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -112,190 +112,180 @@ export const Home = () => {
       <Box width={"100%"} display={"flex"}>
         <Grid container margin={2}>
           <Grid item container spacing={2}>
-            <Grid item xs={12} sm={12} md={6} lg={4} xl={2}>
-              {isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <LinearProgress variant="indeterminate" />
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {!isLoading && countsFound === 0 && counted && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Não foi possivel carregar as estatisticas
+            {isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <LinearProgress variant="indeterminate" />
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {!isLoading && countsFound === 0 && counted && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Não foi possivel carregar as estatisticas
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h3">...</Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {totalCountClientes != null && !isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de Clientes
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h1">{totalCountClientes}</Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {totalCountOrcamentos != null && !isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de Orçamentos
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h1">
+                        {totalCountOrcamentos}
                       </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h3">...</Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {totalCountClientes != null && !isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Total de Clientes
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {totalCountPedidos != null && !isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de Pedidos
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h1">{totalCountPedidos}</Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {totalCountInsumos != null && !isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de Insumos
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h1">{totalCountInsumos}</Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {totalCountProdutos != null && !isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de Produtos
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h1">{totalCountProdutos}</Typography>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {totalCountCategorias != null && !isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de Categorias
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h1">
+                        {totalCountCategorias}
                       </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h1">
-                          {totalCountClientes}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {totalCountOrcamentos != null && !isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Total de Orçamentos
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
+            {totalCountFornecedores != null && !isLoading && (
+              <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Card>
+                  <CardContent>
+                    <Typography variant="h5" align="center">
+                      Total de Fornecedores
+                    </Typography>
+                    <Box
+                      display={"flex"}
+                      padding={6}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                    >
+                      <Typography variant="h1">
+                        {totalCountFornecedores}
                       </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h1">
-                          {totalCountOrcamentos}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {totalCountPedidos != null && !isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Total de Pedidos
-                      </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h1">
-                          {totalCountPedidos}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {totalCountInsumos != null && !isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Total de Insumos
-                      </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h1">
-                          {totalCountInsumos}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {totalCountProdutos != null && !isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Total de Produtos
-                      </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h1">
-                          {totalCountProdutos}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {totalCountCategorias != null && !isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Total de Categorias
-                      </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h1">
-                          {totalCountCategorias}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-              {totalCountFornecedores != null && !isLoading && (
-                <>
-                  <Card>
-                    <CardContent>
-                      <Typography variant="h5" align="center">
-                        Total de Fornecedores
-                      </Typography>
-                      <Box
-                        display={"flex"}
-                        padding={6}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                      >
-                        <Typography variant="h1">
-                          {totalCountFornecedores}
-                        </Typography>
-                      </Box>
-                    </CardContent>
-                  </Card>
-                </>
-              )}
-            </Grid>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
           </Grid>
         </Grid>
       </Box>
