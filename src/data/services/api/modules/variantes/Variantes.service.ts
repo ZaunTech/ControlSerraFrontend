9df9dd2fc6aed2eb8f72b499f0d31dd8267786d3
Henrique Vidotto Vinico Neto <99729533+HenriqueVidotto@users.noreply.github.com>
2023-11-09 22:49:42 -0300

    service Variante

---
+++ src/data/services/api/modules/variantes/Variantes.service.ts
@@ -0,0 +1,120 @@
+import { CreateVarianteDto, IVariante, TListVariantes, UpdateVarianteDto } from ".";
+import { Api, IGetAll } from "../..";
+import { Environment } from "../../../../environment";
+
+
+const rota = "variantes";
+
+const getAll = async (
+  { page = 1,
+    filter = "",
+    perPage = Environment.LIMITE_DE_LINHAS }: Partial<IGetAll> = {}): Promise<TListVariantes | Error> => {
+  try {
+    const urlRelativa = `/${rota}?page=${page}&perPage=${perPage == 0 ? '' : perPage}&titulo_like=${filter}`;
+    const { data, headers } = await Api.get(urlRelativa);
+    if (data) {
+      return {
+        data,
+        totalCount: Number(
+          headers["x-total-count"] || Environment.LIMITE_DE_LINHAS
+        ),
+      };
+    }
+    return new Error(Environment.ERRO_AO_LISTAR_DADOS);
+  } catch (error) {
+    return new Error(
+      (error as { message: string }).message ||
+      Environment.ERRO_AO_ACESSAR_DADOS
+    );
+  }
+};
+const getById = async (id: number): Promise<IVariante | Error> => {
+  try {
+    const urlRelativa = `/${rota}/${id}`;
+    const response = await Api.get<IVariante>(urlRelativa);
+    if (response) {
+      return response.data;
+    }
+    return new Error(Environment.ERRO_AO_LISTAR_DADOS);
+  } catch (error) {
+    console.error(error);
+    return new Error(
+      (error as { message: string }).message ||
+      Environment.ERRO_AO_ACESSAR_DADOS
+    );
+  }
+};
+const create = async (
+  createInsumoDto: CreateVarianteDto
+): Promise<IVariante | Error> => {
+  try {
+    const urlRelativa = `/${rota}`;
+    const response = await Api.post<IVariante>(urlRelativa, createInsumoDto);
+    if (response) {
+      return response.data;
+    }
+    return new Error(Environment.ERRO_AO_LISTAR_DADOS);
+  } catch (error) {
+    return new Error(
+      (error as { message: string }).message ||
+      Environment.ERRO_AO_ACESSAR_DADOS
+    );
+  }
+};
+const updateById = async (
+  id: number,
+  updateInsumoaDto: UpdateVarianteDto
+): Promise<IVariante | Error> => {
+  try {
+    const urlRelativa = `/${rota}/${id}`;
+    const response = await Api.patch(urlRelativa, updateInsumoaDto);
+    if (response.statusText === "OK") {
+      return response.data;
+    }
+    return new Error(Environment.ERRO_AO_LISTAR_DADOS);
+  } catch (error) {
+    return new Error(
+      (error as { message: string }).message ||
+      Environment.ERRO_AO_ACESSAR_DADOS
+    );
+  }
+};
+const deleteById = async (id: number): Promise<IVariante | Error> => {
+  try {
+    const urlRelativa = `/${rota}/${id}`;
+    const response = await Api.delete(urlRelativa);
+    if (response.statusText === "OK") {
+      return response.data;
+    }
+    return new Error(Environment.ERRO_AO_LISTAR_DADOS);
+  } catch (error) {
+    return new Error(
+      (error as { message: string }).message ||
+      Environment.ERRO_AO_ACESSAR_DADOS
+    );
+  }
+};
+const getCount = async (): Promise<number | Error> => {
+  try {
+    const urlRelativa = `/${rota}/count`;
+    const response = await Api.get(urlRelativa);
+    if (response.statusText === "OK") {
+      return response.data;
+    }
+    return new Error(Environment.ERRO_AO_LISTAR_DADOS);
+  } catch (error) {
+    return new Error(
+      (error as { message: string }).message ||
+      Environment.ERRO_AO_ACESSAR_DADOS
+    );
+  }
+};
+
+export const VariantesService = {
+  getAll,
+  getById,
+  create,
+  updateById,
+  deleteById,
+  getCount,
+};
