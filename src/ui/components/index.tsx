7cb8161195d8952bdbe009b57cce5406e33b4a3f
Gabriel Gonçalves <74377524+gaabhenrique@users.noreply.github.com>
2024-09-23 22:32:02 -0300

    Update index.tsx

--- src/ui/components/index.tsx
+++ src/ui/components/index.tsx
@@ -1,8 +1,4 @@
-export { FerramentasDeListagem } from "./ferramentasDeListagem/FerramentasDeListagem.tsx";
-export { SideMenu } from "./navigation/SideMenu.tsx";
-export {
-  FerramentasDeDetalhes,
-  IFerramentasDeDetalhes,
-  TTipo,
-} from "./ferramentasDeDetalhes/FerramentasDeDetalhes.tsx";
+export * from "./ferramentasDeListagem/FerramentasDeListagem.tsx";
 export * from "./ferramentasDeListagem/Actions.tsx";
+export * from "./navigation/SideMenu.tsx";
+export * from "./ferramentasDeDetalhes/FerramentasDeDetalhes.tsx";
