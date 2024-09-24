8af6589dda68112268a138fdbc6559359b8585ea
Gabriel Gonçalves <74377524+gaabhenrique@users.noreply.github.com>
2024-09-23 22:34:25 -0300

    Revert "Update index.tsx"
    This reverts commit 7cb8161195d8952bdbe009b57cce5406e33b4a3f.

--- src/ui/components/index.tsx
+++ src/ui/components/index.tsx
@@ -1,4 +1,8 @@
-export * from "./ferramentasDeListagem/FerramentasDeListagem.tsx";
+export { FerramentasDeListagem } from "./ferramentasDeListagem/FerramentasDeListagem.tsx";
+export { SideMenu } from "./navigation/SideMenu.tsx";
+export {
+  FerramentasDeDetalhes,
+  IFerramentasDeDetalhes,
+  TTipo,
+} from "./ferramentasDeDetalhes/FerramentasDeDetalhes.tsx";
 export * from "./ferramentasDeListagem/Actions.tsx";
-export * from "./navigation/SideMenu.tsx";
-export * from "./ferramentasDeDetalhes/FerramentasDeDetalhes.tsx";
